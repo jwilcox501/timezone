3fa74a1d89a77c83f75449c533b8241971f4864c
John Wilcox <speaktojohn@btinternet.com>
2019-10-21 11:36:34 +0100

    jw03-timezone.txt now available as an embedded resource

--- Timezone/Reader.cs
+++ Timezone/Reader.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,18 @@ namespace Timezone
 {
     class Reader : IReader, IDisposable
     {
+
+        private StringBuilder timeZoneTxt = new StringBuilder();
+
+        public Reader() {
+            GetResouce();
+        }
+
         public List<Tuple<string, string>> Read()
         {
             List<Tuple<string, string>> lReturn = new List<Tuple<string, string>>();
 
-            string[] fileParts = File.ReadAllText("Timezone.txt").Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            string[] fileParts = timeZoneTxt.ToString().Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
 
             foreach (string part in fileParts)
             {
@@ -26,6 +34,14 @@ namespace Timezone
 
             return lReturn;
         }
+
+
+        private void GetResouce() {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            StreamReader reader = new StreamReader(assembly.GetManifestResourceStream("Timezone.Timezone.txt"));
+            timeZoneTxt.Append(reader.ReadToEnd());
+        }
+
         public void Dispose()
         {
         }
